3321eecd475e95faabbdcdea1a26dd81c407bed1
taras-d <taras.datsenko@gmail.com>
2017-05-05 18:51:58 +0300

    Initial app structure

---
+++ src/app/app.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './app.less';
+
+const App = () => <div>APP</div>;
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    entry: './src/root.jsx',
+    output: {
+        filename: 'bundle.js',
+        path: __dirname + '/output'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015', 'react']
+                    }
+                }
+            },
+            {
+                test: /\.less$/,
+                use: ['style-loader', 'css-loader', 'less-loader']
+            }
+        ]
+    },
+    devServer: {
+        publicPath: '/output'
+    }
+}
